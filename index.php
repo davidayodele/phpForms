6350f9360d18283ab6cc28ba0837f96aa433d5b3
Number10 <starcanyonschool@gmail.com>
2019-12-19 22:25:41 -0500

    edited index

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ echo('============ Arrays ==============<br>');
 #Indexed
 $names = array('Alice', 'Bob', 'Charlie', 'Dan', 'Ellen');
 $cars = ['Acura', 'BMW', 'Chevy', 'Dodge'];
-
+$prices = [49567.54, 89432.65, 22437.77, 36859.19, 19840.30];
 print($names[0]);
 print("<br>");
 echo($cars[3]);
@@ -39,11 +39,80 @@ print("<br><br>");
 $cars[4] = 'Ford';
 var_dump($cars); // does not throw error since cars[4] is now defined
 print("<br><br>");
+var_dump($prices);
+print("<br><br>");
 #Associative
 $driver = ['Alice' => 'Acura', 'Bob' => 'BMW', 'Charlie' => 'Chevy'];
 echo($driver['Charlie']);
 print("<br>");
 print_r($driver);
+print("<br><br>");
+
+$order = array(
+  'Alice' => array('Car' => 'Acura', 'Price' => 49567.54, 'MPG' => 31),
+  'Bob' => array('Car' => 'BMW', 'Price' => 89432.65, 'MPG' => 27),
+  'Charlie' => array('Car' => 'Chevy', 'Price' => 36859.19, 'MPG' => 33),
+  'Dan' => array('Car' => 'Dodge', 'Price' => 22437.77, 'MPG' => 34),
+);
+
+$car1 = ['Car' => 'Acura', 'Price' => 49567.54, 'MPG' => 31];
+$car2 = ['Car' => 'BMW', 'Price' => 89432.65, 'MPG' => 27];
+$car3 = ['Car' => 'Chevy', 'Price' => 36859.19, 'MPG' => 33];
+$car4 = ['Car' => 'Dodge', 'Price' => 22437.77, 'MPG' => 34];
+
+$cars2  = array (
+  ['Car' => 'Acura', 'Price' => 49567.54, 'MPG' => 31],
+  ['Car' => 'BMW', 'Price' => 89432.65, 'MPG' => 27],
+  ['Car' => 'Chevy', 'Price' => 36859.19, 'MPG' => 33],
+  ['Car' => 'Dodge', 'Price' => 22437.77, 'MPG' => 34],
+  ['Car' => 'Ford', 'Price' => 19840.30, 'MPG' => 30]
+);
+
+$order2 = array(
+  $names[0] => $car1,
+  $names[1] => $car2,
+  $names[2] => $car3,
+  $names[3] => $car4,
+);
+
+/*
+$order3 = array (
+  for ($i = 0; $i <= 4; $i++){
+    $names[$i] => $cars2[$i];
+  }
+);
+*/
+
+$order3 = array();
+for ($i = 0; $i <= 4; $i++){
+   array_push($order3, $names[$i], $cars2[$i]);
+}
+
+//$order3 = foreach ($order3 as $names => $cars2);
+
+$order4 = array_map(null, $names, $cars2);
+
+echo("Order array: <br>");
+print_r($order3);
+print("<br><br>");
+echo("Alice's MPG: <br>");
+print($order['Alice']['MPG']);
+print("<br><br>");
+echo("Order array2: <br>");
+print_r($order2);
+print("<br><br>");
+echo("Dan's Car: <br>");
+print($order2['Dan']['Car']);
+print("<br><br>");
+print_r($order3);
+print("<br><br>");
+echo("Charlie's price: <br>");
+print_r($order3[4]);
+print("<br><br>");
+print_r($order4);
+print("<br><br>");
+echo("Ellen's price: <br>");
+print_r($order4[4][1]['Price']);
 echo('<br>============ End Arrays ============');
 
 //======= Loops ===============
@@ -51,16 +120,65 @@ print("<br>");
 echo('============ Loops ==============<br>');
 /*=====For Loops=====
 - Cases: Definite, iteration length is known
-- Params: initializer, condition, increment
-- Flow: Initialization -> Condition check -> Event execution -> Interation
+- Params: explicit initializer, explicit condition, explicit increment
+- Flow: Initialization -> Condition check -> Event execution -> Iteration
 */
-print('<br>')
-for($i = 0; i < 10; $i++){
-  if($i % 2 ==0 ){
-      echo($i);
+print('<br>');
+for($i = 0; $i < 10; $i++){
+  if($i % 2 == 0){
+      echo $i;
   }
 }
-print('<br>')
-echo('<br>============ End Loops ============');
+print('<br>');
 
+/*=====While Loops=====
+- Cases: Indefinite, iteration length is unknown
+- Params: implicit initializer, explicit condition, implicit increment
+- Flow: Condition check -> Event execution (typically forces iteration)
+*/
+print('<br>');
+$counts = [0, 3, 8];
+while($counts[0] < $counts[2]){
+  if($counts[0] % 2 == 0){
+      print_r($counts);
+      print('<br>');
+  }
+  ++$counts[0]; //prefix iteration (execution first)
+}
+print('<br>');
+
+/*=====Do While Loops=====
+- Cases: Indefinite, iteration length is unknown
+- Params: implicit initializer, explicit condition, implicit increment
+- Flow: Event execution (typically forces iteration) -> Condition check
+*/
+print('<br>');
+$counts2 = [1, 5, 9];
+do {
+  if($counts2[2] % 2 != 0){
+      print_r($counts2);
+      print('<br>');
+  }
+  $counts2[2]--; //prefix iteration (execution first)
+} while ($counts2[2] >= $counts2[0]);
+
+print('<br>');
+
+/*=====For Each Loops=====
+- Cases: Definite, iteration length is unknown
+- Params: Implicit initializer, implicit condition, explicit increment
+- Flow: Iteration -> Event execution
+*/
+print('<br>');
+$counts2 = [1, 5, 9];
+do {
+  if($counts2[2] % 2 != 0){
+      print_r($counts2);
+      print('<br>');
+  }
+  $counts2[2]--; //prefix iteration (execution first)
+} while ($counts2[2] >= $counts2[0]);
+
+print('<br>');
+echo('<br>============ End Loops ============');
 ?>
