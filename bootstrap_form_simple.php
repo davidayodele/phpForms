b4283ec459b189417ad978cdbbfece7cc12c939c
Number10 <starcanyonschool@gmail.com>
2019-12-28 23:11:03 -0500

    added site3

---
+++ bootstrap_form_simple.php
@@ -0,0 +1,22 @@
+<form>
+<div class="form-group">
+<label>
+Name
+</label>
+<input type="text" name="name" class="form-control" value="">
+</div>
+<div class="form-group">
+<label>
+Email
+</label>
+<input type="text" name="email" class="form-control" value="">
+</div>
+<div class="form-group">
+<label>
+Message
+</label>
+<input type="text" name="msg" class="form-control" value="">
+</div>
+<br>
+<button type="submit" name="submit" class="btn btn-primary">Submit</button>
+</form>
---
+++ bootstrap_navbar_simple.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-default">
+<div class="container">
+<div class="navbar-header">
+<a class="navbar-brand" href="index.php">Site 3</a>
+</div>
+</div>
+</nav>
