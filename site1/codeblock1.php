e1cccd2f7ce226fb721794e17cf97f214589c539
davidayodele <starcanyonschool@gmail.com>
2020-01-13 00:37:03 -0700

    added codeblocks1

---
+++ site1/codeblock1.php
@@ -0,0 +1,149 @@
+<style type="text/css">
+
+/* ==== Navbar Logo ==== */
+@media only screen and (max-width: 989px) and (min-width: 768px)
+.responsive .logo {
+    float: left;
+    height: 57px;
+    margin-top: 15px;
+}
+
+@media only screen and (max-width: 767px)
+.responsive .logo img {
+    max-height: 55px !important;
+}
+
+/* Need to set for for scroll down
+.logo, .logo a {
+    overflow: hidden;
+    position: relative;
+    display: block;
+    height: 100%;
+    box-shadow: 0px 1px 7px 0.5px rgba(0, 0, 0, 0.33);
+    border-radius: 50%;
+}
+*/
+/* ==== End Navbar Logo ==== */
+
+/* ==== Floating Logo & text ==== */
+@media only screen and (min-width: 691px) {
+    a.avia-slideshow-button.avia-button.avia-color-red.avia-multi-slideshow-button.avia-slideshow-button-2 {
+        margin-left: 25px;
+    }
+}
+
+.avia-caption-content img {
+    box-shadow: 1px 1px 10px 1px rgba(0, 0, 0, 0.33);
+    border-radius: 50%;
+    width: 150px;
+    margin-left: 15px;
+}
+
+.avia-caption-content p {
+    text-shadow: 2px 2px 7px rgba(0, 0, 0, 0.9);
+    width: 330px;
+}
+
+h2.avia-caption-title {
+    text-shadow: 1px 1px 7px rgba(0, 0, 0, 0.43);
+    width: 355px;
+}
+
+@media only screen and (max-width: 691px) {
+	h2.avia-caption-title {
+	    margin-left: 15px;
+	}
+
+	.avia-caption-content p {
+	    width: 310px;
+	}
+}
+ 
+/* ==== End Floating Logo & text ==== */
+
+.av-special-heading.avia-builder-el-no-sibling {
+    margin-top: 0px;
+    margin-bottom: 0px;
+    width: fit-content;
+    margin: auto;
+    background-color: rgba(0, 0, 0, 0.67);
+    /* margin: 0px 0px 0px 0px; */
+}
+
+.av-special-heading.av-special-heading-h2.blockquote.modern-quote.avia-builder-el-18.avia-builder-el-no-sibling.av-thin-font {
+    background-color: white;
+}
+
+.av-special-heading.av-special-heading-h3.avia-builder-el-57.el_before_av_google_map.avia-builder-el-no-sibling {
+    background-color: rgba(255, 255, 255, 1);
+}
+
+/*===== Calendars ====*/
+/* 
+[calendar id="3310"]
+[calendar id="3683"]
+*/
+.alternate_color .template-archives .tabcontainer a, #top .alternate_color .tabcontainer .tab:hover, #top .alternate_color .tabcontainer .tab.active_tab {
+    color: #fff;
+}
+
+.alternate_color .sidebar_tab, .alternate_color .tabcontainer .tab {
+    color: #fff;
+}
+
+div .alternate_color .tabcontainer .active_tab_content, div .alternate_color .tabcontainer .active_tab {
+    background-color: #342f29;
+    color: #fff;
+}
+
+.alternate_color table caption, .alternate_color tr:nth-child(even), .alternate_color .pricing-table>li:nth-child(even), #top .alternate_color .avia-data-table.avia_pricing_minimal td {
+    color: #dadada;
+}
+
+/* #fbf301 */
+
+b {
+    color: #E8BF4E !important;
+}
+
+.simcal-default-calendar-light .simcal-nav-button {
+    color: rgba(255,255,255,.5);
+}
+
+.simcal-default-calendar-light .simcal-nav-button {
+    color: rgba(255,255,255,.5);
+}
+
+.simcal-default-calendar-dark .simcal-nav-button:disabled, .simcal-default-calendar-dark .simcal-nav-button:disabled:hover {
+    color: rgba(255,255,255,.5)!important;
+}
+
+.simcal-default-calendar-dark .simcal-nav-button:disabled, .simcal-default-calendar-dark .simcal-nav-button:disabled:hover {
+    color: rgba(255,255,255,.6)!important;
+}
+/*===== End Calendars ====*/
+
+
+/*===== Contact form ====*/
+first_form#top label {
+    font-weight: bold;
+    font-size: 0.92em;
+    color: #dadada;
+}
+/*===== End Contact form ====*/
+
+/*===== Address info ====*/
+avia-builder-el-41.iconbox_content_container {
+    color: #dadada;
+}
+/*===== End Address info ====*/
+
+/*===== Google map area ====*/
+.av-special-heading.av-special-heading-h3.avia-builder-el-59.el_before_av_google_map.avia-builder-el-no-sibling {
+    background-color: #fff;
+}
+
+/*===== End Google map area ====*/
+
+</style>
+
