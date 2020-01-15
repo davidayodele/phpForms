d0913c08f9902a493130eacaef5823ecefd67cef
davidayodele <starcanyonschool@gmail.com>
2020-01-14 23:10:52 -0800

    added codeblock1

---
+++ codeblock1_pac.php
@@ -0,0 +1,48 @@
+
+
+#top #main .avia-section .template-page {
+    margin-top: -35px;
+}
+
+/*==== Header & Nav ====*/
+.logo img {
+    /*
+    padding: 0;
+    display: block;
+    width: auto;
+    height: auto;
+    max-height: 100%;
+    image-rendering: auto;
+    position: relative;
+    z-index: 2;
+    height: 100%\9;
+    height: auto\9;
+    -webkit-transition: opacity 0.4s ease-in-out;
+    transition: opacity 0.4s ease-in-out;
+    */
+    padding: 5px 0px 7px 0px;
+    ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=2, OffY=2, Color='#000')";
+    filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=2, OffY=2, Color='#000')";
+    filter: drop-shadow(0.5px 1px 2px rgba(0,0,0,0.33));
+}
+
+.header_color .av-hamburger-inner, .header_color .av-hamburger-inner::before, .header_color .av-hamburger-inner::after {
+    background-color: #fff;
+}
+
+#top .av_header_glassy.av_header_transparency .header_bg {
+    background-color: #000 !important;
+    opacity: 0.1;
+    filter: alpha(opacity=10);
+}
+
+.html_header_transparency #top .avia-builder-el-0 .container, .html_header_transparency #top .avia-builder-el-0 .slideshow_caption {
+    padding-top: 0px;
+    margin-top: -70px;
+}
+
+.av_minimal_header_shadow {
+    box-shadow: 0 0 2px 1px rgba(0,0,0,0.1) !important;
+}
+/*==== End Header & Nav ====*/
+
