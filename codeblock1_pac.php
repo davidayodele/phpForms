a9346b8fffc6231f6acb9779deb94d956e21c969
davidayodele <starcanyonschool@gmail.com>
2020-02-17 13:20:21 -0700

    edited codeblock1_pac.php

--- codeblock1_pac.php
+++ codeblock1_pac.php
@@ -21,51 +21,10 @@ AMTI Bookmark! -->
 
 
 5) canyons-382549197 1) downtown-384405427 2) entrance-384669026 3) actv-384674057 4) floor-384675996,384908547
-6) applause-384689797 7) Phoenix - 391044596
-
+6) applause-384689797 
 -->
 
 */
-
-
-/*==== Mobile section 1 ====*/
-@media only screen and (max-width: 990px) {
-    .avia-builder-el-first .av-section-color-overlay-wrap {
-        top: -20px;
-    }
-
-    .avia-builder-el-first .av-section-color-overlay-wrap {
-        margin-top: -160px;
-    }
-}
-
-
-@media only screen and (max-width: 768px) {
-
-    .avia-builder-el-first .av-section-color-overlay-wrap {
-        margin-top: 0px;
-    }
-}
-
-@media only screen and (max-width: 692px) {
-
-    .avia-section-large .content, .avia-section-large .sidebar {
-        padding-top: 0px !important;
-    }
-
-    span.hr-inner.inner-border-av-border-thin {
-        width: 140px !important;
-    }
-
-    #top h1 strong, #top h2 strong, #top h3 strong, #top h4 strong, #top h5 strong, #top h6 strong {
-
-        font-size: 12.5vw !important;
-    }
-
-}
-
-/*==== End Mobile section 1 ====*/
-
 
 /*==== Header, nav, & line menu ====*/
 #top .av_header_glassy.av_header_transparency .social_bookmarks li, #top .av_header_glassy.av_header_transparency .social_bookmarks li a {
@@ -86,8 +45,8 @@ AMTI Bookmark! -->
     image-rendering: auto;
     position: relative;
     z-index: 2;
-    height: 100%\9;
-    height: auto\9;
+    height: 100%9;
+    height: auto9;
     -webkit-transition: opacity 0.4s ease-in-out;
     transition: opacity 0.4s ease-in-out;
     */
@@ -152,26 +111,11 @@ div#av_section_2 {
     background-color: #f8f8f8;
 }
 
-/* == FB codeblock ==*/
-<script async="" defer="" crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2">
-</script>
-
-<div class="fb-page fb_iframe_widget" data-href="https://www.facebook.com/nowpacaz" data-tabs="timeline, events" data-width="500" data-height="500" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" fb-xfbml-state="rendered" fb-iframe-plugin-query="adapt_container_width=true&app_id=&container_width=501&height=400&hide_cover=false&href=https%3A%2F%2Fwww.facebook.com%2Fnowpacaz&locale=en_US&sdk=joey&show_facepile=false&small_header=true&tabs=timeline%2C%20events&width=500"><span style="vertical-align: bottom; width: 500px; height: 500px;"><iframe name="f249e4acced1964" width="500px" height="500px" title="fb:page Facebook Social Plugin" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" src="https://www.facebook.com/v3.2/plugins/page.php?adapt_container_width=true&app_id=&channel=https%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D45%23cb%3Df3f9683534b2904%26domain%3Daznowpac.org%26origin%3Dhttp%253A%252F%252Faznowpac.org%252Ff3300a944931ccc%26relation%3Dparent.parent&container_width=501&height=400&hide_cover=false&href=https%3A%2F%2Fwww.facebook.com%2Fnowpacaz&locale=en_US&sdk=joey&show_facepile=false&small_header=true&tabs=timeline%2C%20events&width=500" style="border: none; visibility: visible; width: 500px; height: 500px;" class=""></iframe></span></div>
-/* == End FB codeblock ==*/
-
-/* == Twitter codeblock ==*/
-<a class="twitter-timeline" data-height="500" href="https://twitter.com/NOWPACAZ?ref_src=twsrc%5Etfw">Tweets by NOWPACAZ</a> 
-
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-
-/*== End Twitter codeblock ==*/
-
 @media only screen and (min-width: 615px) {
     .fb-page.fb_iframe_widget {
         margin-left: 12%;
     }
 }
-
 .avia-builder-el-11 .av-subheading p:last-child {
     color: #000 !important;
 }
@@ -185,6 +129,7 @@ div#av_section_2 {
     margin-bottom: 0;
     font-weight: 400 !important;
 }
+
 .av-masonry-entry .av-masonry-entry-title {
     font-weight: 400 !important;
     color: #fff !important;
@@ -205,10 +150,9 @@ div#av_section_2 {
     background-color: rgba(0, 0, 0, 0.65) !important;
 }
 
-figcaption.av-inner-masonry-content.site-background:hover {
-    background-color: rgba(0, 0, 0, 0.33) !important;
-}
 /*==== End Endorsements ====*/
+
+
 
 
 /*==== Endorsement Process ====*/
@@ -254,7 +198,11 @@ figcaption.av-inner-masonry-content.site-background:hover {
     font-weight: 400;
 }
 
-/*=== Core issues text ===*/
+#service .av_textblock_section .avia_textblock h2 {
+    font-size: 1.3rem;
+}
+
+/*=== Core issues text only ===*/
 .avia-builder-el-11 p {
     font-weight: normal;
     color: #fff;
@@ -343,6 +291,12 @@ figcaption.av-inner-masonry-content.site-background:hover {
     text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.33);
 }
 
+.avia-builder-el-29 p {
+    font-weight: normal;
+    color: #fff;
+    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.33);
+}
+
 .avia-builder-el-32 p {
     font-weight: normal;
     color: #fff;
@@ -367,14 +321,26 @@ figcaption.av-inner-masonry-content.site-background:hover {
     text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.33);
 }
 
+.avia-builder-el-45 p {
+    font-weight: normal;
+    color: #fff;
+    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.33);
+}
+
 .avia-builder-el-48 p {
+    font-weight: normal;
+    color: #fff;
+    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.33);
+}
+
+.avia-builder-el-49 p {
     font-weight: normal;
     color: #fff;
     text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.33);
 }
 /*=== End Core Issues Text ===*/
 
-/*=== Core issues icons ===*/
+/*=== Core issues icons only ===*/
 .avia-builder-el-11 .av-icon-char {
     color: #fff;
     ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
@@ -459,26 +425,6 @@ figcaption.av-inner-masonry-content.site-background:hover {
     filter: drop-shadow(0.5px 1.5px 0.5px rgba(0,0,0,0.65));
 }
 
-.avia-builder-el-40 .av-icon-char {
-    color: #fff;
-    ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
-    filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
-    filter: drop-shadow(0.5px 1.5px 0.5px rgba(0,0,0,0.65));
-}
-
-.avia-builder-el-44 .av-icon-char {
-    color: #fff;
-    ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
-    filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
-    filter: drop-shadow(0.5px 1.5px 0.5px rgba(0,0,0,0.65));
-}
-
-.avia-builder-el-48 .av-icon-char {
-    color: #fff;
-    ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
-    filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
-    filter: drop-shadow(0.5px 1.5px 0.5px rgba(0,0,0,0.65));
-}
 .avia-builder-el-37 .av-icon-char {
     font-weight: normal;
     color: #fff;
@@ -489,6 +435,13 @@ figcaption.av-inner-masonry-content.site-background:hover {
     font-weight: normal;
     color: #fff;
     text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.33);
+}
+
+.avia-builder-el-40 .av-icon-char {
+    color: #fff;
+    ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
+    filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
+    filter: drop-shadow(0.5px 1.5px 0.5px rgba(0,0,0,0.65));
 }
 
 .avia-builder-el-41 .av-icon-char {
@@ -504,13 +457,36 @@ figcaption.av-inner-masonry-content.site-background:hover {
     text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.33);
 }
 
+.avia-builder-el-44 .av-icon-char {
+    color: #fff;
+    ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
+    filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
+    filter: drop-shadow(0.5px 1.5px 0.5px rgba(0,0,0,0.65));
+}
+
 .avia-builder-el-45 .av-icon-char {
     color: #fff;
     ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
     filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
     filter: drop-shadow(0.5px 1.5px 0.5px rgba(0,0,0,0.65));
 }
+
+.avia-builder-el-48 .av-icon-char {
+    color: #fff;
+    ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
+    filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
+    filter: drop-shadow(0.5px 1.5px 0.5px rgba(0,0,0,0.65));
+}
+
+.avia-builder-el-49 .av-icon-char {
+    color: #fff;
+    ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
+    filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=0.5, OffY=0.5, Color='#000')";
+    filter: drop-shadow(0.5px 1.5px 0.5px rgba(0,0,0,0.65));
+}
 /*=== End Core issues icons ===*/
+
+/*=== Core issues bg only ===*/
 
 .flex_column.av_one_third.av-animated-generic.right-to-left.flex_column_div.av-zero-column-padding.first.avia-builder-el-37.el_after_av_one_third.el_before_av_one_third.column-top-margin.avia_start_animation.avia_start_delayed_animation {
     background-color: rgba(0, 0, 0, 0.65);
@@ -530,7 +506,7 @@ to-left.flex_column_div.av-zero-column-padding.avia-builder-el-41.el_after_av_on
     box-shadow: 0.5px 0.5px 5px 1px rgba(0, 0, 0, 0.33);
 }
 
-.flex_column.av_one_third.av-animated-generic.right-to-left.flex_column_div.av-zero-column-padding.avia-builder-el-29.el_after_av_one_third.el_before_av_one_third.avia_start_animation.avia_start_delayed_animation {
+.flex_column.av_one_third.av-animated-generic.right-to-left.flex_column_div.av-zero-column-padding.first.avia-builder-el-29.el_after_av_heading.el_before_av_one_third.avia_start_animation.avia_start_delayed_animation {
     background-color: rgba(0, 0, 0, 0.65);
     padding: 15px 5px 0px 15px;
     box-shadow: 0.5px 0.5px 5px 1px rgba(0, 0, 0, 0.33);
@@ -578,7 +554,7 @@ to-left.flex_column_div.av-zero-column-padding.avia-builder-el-41.el_after_av_on
     box-shadow: 0.5px 0.5px 5px 1px rgba(0, 0, 0, 0.33);
 }
 
-.flex_column.av_one_third.av-animated-generic.right-to-left.flex_column_div.av-zero-column-padding.avia-builder-el-45.el_after_av_one_third.avia-builder-el-last.column-top-margin.avia_start_animation.avia_start_delayed_animation {
+.flex_column.av_one_third.av-animated-generic.right-to-left.flex_column_div.av-zero-column-padding.avia-builder-el-45.el_after_av_one_third.el_before_av_one_third.column-top-margin.avia_start_animation.avia_start_delayed_animation {
     background-color: rgba(0, 0, 0, 0.65);
     padding: 15px 5px 0px 15px;
     box-shadow: 0.5px 0.5px 5px 1px rgba(0, 0, 0, 0.33);
@@ -592,6 +568,12 @@ to-left.flex_column_div.av-zero-column-padding.avia-builder-el-41.el_after_av_on
 
 
 .flex_column.av_one_third.av-animated-generic.right-to-left.flex_column_div.av-zero-column-padding.first.avia-builder-el-28.el_after_av_heading.el_before_av_one_third.avia_start_animation.avia_start_delayed_animation {
+    background-color: rgba(0, 0, 0, 0.65);
+    padding: 15px 5px 0px 15px;
+    box-shadow: 0.5px 0.5px 5px 1px rgba(0, 0, 0, 0.33);
+}
+
+.flex_column.av_one_third.av-animated-generic.right-to-left.flex_column_div.av-zero-column-padding.avia-builder-el-29.el_after_av_one_third.avia-builder-el-last.column-top-margin.avia_start_animation.avia_start_delayed_animation {
     background-color: rgba(0, 0, 0, 0.65);
     padding: 15px 5px 0px 15px;
     box-shadow: 0.5px 0.5px 5px 1px rgba(0, 0, 0, 0.33);
@@ -639,11 +621,26 @@ to-left.flex_column_div.av-zero-column-padding.avia-builder-el-41.el_after_av_on
     box-shadow: 0.5px 0.5px 5px 1px rgba(0, 0, 0, 0.33);
 }
 
+.flex_column.av_one_third.av-animated-generic.right-to-left.flex_column_div.av-zero-column-padding.avia-builder-el-45.el_after_av_one_third.avia-builder-el-last.column-top-margin.avia_start_animation.avia_start_delayed_animation {
+    background-color: rgba(0, 0, 0, 0.65);
+    padding: 15px 5px 0px 15px;
+    box-shadow: 0.5px 0.5px 5px 1px rgba(0, 0, 0, 0.33);
+}
+
 .flex_column.av_one_third.av-animated-generic.right-to-left.flex_column_div.av-zero-column-padding.avia-builder-el-48.el_after_av_one_third.avia-builder-el-last.column-top-margin.avia_start_animation.avia_start_delayed_animation {
     background-color: rgba(0, 0, 0, 0.65);
     padding: 15px 5px 0px 15px;
     box-shadow: 0.5px 0.5px 5px 1px rgba(0, 0, 0, 0.33);
 }
+
+.flex_column.av_one_third.av-animated-generic.right-to-left.flex_column_div.av-zero-column-padding.avia-builder-el-49.el_after_av_one_third.avia-builder-el-last.column-top-margin.avia_start_animation.avia_start_delayed_animation {
+    background-color: rgba(0, 0, 0, 0.65);
+    padding: 15px 5px 0px 15px;
+    box-shadow: 0.5px 0.5px 5px 1px rgba(0, 0, 0, 0.33);
+}
+
+/*=== End Core issues bg only ===*/
+
 
 .avia-builder-el-33 p {
     color: #fff;
@@ -677,67 +674,6 @@ to-left.flex_column_div.av-zero-column-padding.avia-builder-el-41.el_after_av_on
 }
 /*==== End Core Issues Section ====*/
 
-/*==== TV Stream ====*/
-div#player {
-    width: 100% !important;
-}
-
-#player-wrap {
-    width: 100%;
-}
-
-#player-section {
-    float: left;
-    width: 92% !important;
-    background-color: transparent !important;
-}
-/*==== End TV Stream ====*/
-
-/*==== Calendar ====*/
-
-/*---- Featured Event ----*/
-<br>
-<hr>
-<iframe src="https://docs.google.com/viewer?url=http://aznow.org/wp-content/uploads/2020/02/Equal-Rights-amendment-task-force-AZ-invites-you-to.pdf&amp;embedded=true" width="100%" height="500px" frameborder="0">
-</iframe>
-<hr>
-<br>
-/*---- End Featured Event ----*/
-
-span.simcal-day-label.simcal-day-number {
-    font-weight: normal;
-}
-
-span.simcal-event-title {
-    font-weight: normal;
-}
-
-span.simcal-event-address.simcal-event-start-location {
-    font-weight: normal;
-}
-
-.simcal-calendar.simcal-default-calendar.simcal-default-calendar-grid.simcal-default-calendar-dark {
-    font-weight: normal !important;
-    box-shadow: 0.5px 0.5px 3px 0.5px rgba(0, 0, 0, 0.33);
-}
-
-.simcal-event-bubble .simcal-event-details .simcal-event-description, .simcal-event-bubble .simcal-event-details p {
-    font-weight: normal;
-}
-/*==== End Calendar ====*/
-
-/*==== Footers ====*/
-.textwidget {
-    font-weight: normal !important;
-}
-
-#socket .copyright {
-    float: left;
-    font-weight: normal;
-}
-/*==== End Footers ====*/
-
-
 
 /*
 @media max-width: 660px
@@ -746,4 +682,3 @@ span.simcal-event-address.simcal-event-start-location {
 }
 */
 </style>
-
