21583d6461d8bc2a18a3bb5fa3884281ba2173a8
davidayodele <starcanyonschool@gmail.com>
2020-02-27 16:42:06 -0700

    added codeblock2 pac, right block

---
+++ codeblock2_pac.php
@@ -0,0 +1,194 @@
+<a class="twitter-timeline" data-width="500" data-height="500" href="https://twitter.com/NOWPACAZ?ref_src=twsrc%5Etfw">Tweets by NOWPACAZ</a> 
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<style type="text/css">
+/*
+.ls-wrapper.find_my_leg:hover {
+    background: #ba5400 !important;
+    box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.33) !important;
+}
+
+div.ls-wrapper > div.ls-wrapper > p.find_leg_btn {
+     background-color: #ba5400 !important;
+}
+
+#find_leg_btn:hover {
+    background: #ba5400 !important;
+    box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.33) !important;
+}
+
+[data-slide-index="2"] p#find_my_leg_btn:focus {
+    background: #ba5400 !important;
+    box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.33) !important;
+}
+
+[data-slide-index="2"] p#find_my_leg_btn:active {
+    background: #ba5400 !important;
+    box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.33) !important;
+}
+
+span#fb_icon:hover {
+    opacity: 0.50 !important;
+}
+
+span#tw_icon:hover {
+    opacity: 0.50 !important;
+}
+
+p#view-all_btn:hover {
+    opacity: 1 !important;
+}
+
+*/
+
+/*==== Slider ====*/
+#action_btn:hover {
+    background: #c93a3d !important;
+    box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.33) !important;
+}
+
+#layerslider_1 > div.ls-inner > div.ls-layers > div.ls-wrapper > div.ls-wrapper > p.find_leg_btn:hover {
+    background: #ba5400 !important;
+    box-shadow: 1px 1px 7px 2px rgba(0, 0, 0, 0.33) !important;
+}
+
+img#donate_btn:hover {
+    box-shadow: 1px 1px 4px 1px rgba(0, 0, 0, 0.33) !important;
+    border-radius: 10px !important;
+}
+
+#signup_btn:hover {
+    box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.33) !important;
+}
+
+/*==== End Slider ====*/
+
+
+/*==== Endorsements ====*/
+#endorsements .avia-builder-el-19 .av-subheading p:last-child {
+    margin-bottom: 0;
+    font-weight: 400 !important;
+    color: #000;
+    text-shadow: none;
+}
+
+.avia-builder-el-19 .av-subheading p:last-child {
+    margin-bottom: 0;
+    font-weight: 400 !important;
+    color: #000;
+    text-shadow: none;
+}
+
+#endorsements .av-subheading p:first-child {
+    margin-top: 0;
+    color: #000 !important;
+    text-shadow: none;
+}
+/*==== End Endorsements ====*/
+
+/*==== Mobile section 1 ====*/
+@media only screen and (max-width: 990px) {
+    .avia-builder-el-first .av-section-color-overlay-wrap {
+        top: -20px;
+    }
+
+    .avia-builder-el-first .av-section-color-overlay-wrap {
+        margin-top: -160px;
+    }
+}
+
+
+@media only screen and (max-width: 768px) {
+
+    .avia-builder-el-first .av-section-color-overlay-wrap {
+        margin-top: 0px;
+    }
+}
+
+@media only screen and (max-width: 692px) {
+
+    .avia-section-large .content, .avia-section-large .sidebar {
+        padding-top: 0px !important;
+    }
+
+    span.hr-inner.inner-border-av-border-thin {
+        width: 140px !important;
+    }
+
+    #top h1 strong, #top h2 strong, #top h3 strong, #top h4 strong, #top h5 strong, #top h6 strong {
+
+        font-size: 12.5vw !important;
+    }
+
+}
+
+/*==== End Mobile section 1 ====*/
+
+/*==== Calendar ====*/
+span.simcal-day-label.simcal-day-number {
+    font-weight: normal;
+}
+
+span.simcal-event-title {
+    font-weight: normal;
+}
+
+span.simcal-event-address.simcal-event-start-location {
+    font-weight: normal;
+}
+
+.simcal-calendar.simcal-default-calendar.simcal-default-calendar-grid.simcal-default-calendar-dark {
+    font-weight: normal !important;
+    /*box-shadow: 0.5px 0.5px 3px 0.5px rgba(0, 0, 0, 0.33);*/
+}
+
+.simcal-event-bubble .simcal-event-details .simcal-event-description, .simcal-event-bubble .simcal-event-details p {
+    font-weight: normal;
+}
+/*==== End Calendar ====*/
+
+/*==== Endorsements ====*/
+#endorsements span {
+    font-weight: 400;
+    font-size: smaller;
+}
+
+figcaption.av-inner-masonry-content.site-background:hover {
+    background-color: rgba(0, 0, 0, 0.33) !important;
+}
+/*==== End Endorsements ====*/
+
+/*==== Endorsement Process ====*/
+.avia-builder-el-51 .avia_textblock {
+    font-weight: normal;
+}
+
+.avia-builder-el-55 .avia_textblock {
+    font-weight: normal;
+}
+
+.avia-builder-el-59 .avia_textblock {
+    font-weight: normal;
+}
+
+.main_color .hr-short .hr-inner-style, .main_color .hr-short .hr-inner {
+    background-color: #000;
+}
+
+.main_color, .main_color div, .main_color header, .main_color main, .main_color aside, .main_color footer, .main_color article, .main_color nav, .main_color section, .main_color span, .main_color applet, .main_color object, .main_color iframe, .main_color h1, .main_color h2, .main_color h3, .main_color h4, .main_color h5, .main_color h6, .main_color p, .main_color blockquote, .main_color pre, .main_color a, .main_color abbr, .main_color acronym, .main_color address, .main_color big, .main_color cite, .main_color code, .main_color del, .main_color dfn, .main_color em, .main_color img, .main_color ins, .main_color kbd, .main_color q, .main_color s, .main_color samp, .main_color small, .main_color strike, .main_color strong, .main_color sub, .main_color sup, .main_color tt, .main_color var, .main_color b, .main_color u, .main_color i, .main_color center, .main_color dl, .main_color dt, .main_color dd, .main_color ol, .main_color ul, .main_color li, .main_color fieldset, .main_color form, .main_color label, .main_color legend, .main_color table, .main_color caption, .main_color tbody, .main_color tfoot, .main_color thead, .main_color tr, .main_color th, .main_color td, .main_color article, .main_color aside, .main_color canvas, .main_color details, .main_color embed, .main_color figure, .main_color fieldset, .main_color figcaption, .main_color footer, .main_color header, .main_color hgroup, .main_color menu, .main_color nav, .main_color output, .main_color ruby, .main_color section, .main_color summary, .main_color time, .main_color mark, .main_color audio, .main_color video, #top .main_color .pullquote_boxed, .responsive #top .main_color .avia-testimonial, .responsive #top.avia-blank #main .main_color.container_wrap:first-child, #top .main_color.fullsize .template-blog .post_delimiter, .main_color .related_posts.av-related-style-full a {
+    border-color: unset;
+}
+/*==== Endorsement Process ====*/
+
+
+/*==== Footers ====*/
+.textwidget {
+    font-weight: normal !important;
+}
+
+#socket .copyright {
+    float: left;
+    font-weight: normal;
+}
+/*==== End Footers ====*/
+</style>
